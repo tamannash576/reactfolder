476ffd8fe7ac0312897fb0838f7e177dbafce70b
tamannash576 <tamanna.sharma0704@gmail.com>
2025-07-28 19:18:11 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import React, { useInsertionEffect, useState } from 'react'
+
+function App() {
+  /*const student=[{ name:'Tamanna sharma', age:22},
+   {name:'anuj sharma', age:27},
+   {name:'avisha sharma', age:27},
+   {name:'viaan sharma', age:27},
+  
+  ]
+  
+  // map ex
+  
+  const name=student.map(student=>student.name);
+  console.log(name)
+  return (
+   <div>
+     <h1>Student Names:</h1>
+     <ul>
+       {name.map((name, index) => (
+         <li key={index}>{name}</li>
+       ))}
+     </ul>
+   </div>
+  );*/
+
+
+  //filter ex
+
+
+  /*const Usersage = student.filter(s => s.age);
+  console.log(Usersage)*/
+  // find
+  /*const user = student.find(user => user.age===22);
+  console.log(user)*/
+
+
+  // some
+
+
+  /*const scores = [10, 20, 30];
+  const High = scores.some(score => score > 25);
+  console.log(High)*/
+
+  //includes
+
+
+  /*const fruits= ["apple", "banana","mango","cherry"];
+  const Selected = fruits.includes("banana");*/
+  //console.log(Selected)
+
+
+  //indexof
+
+
+  /*const current = "banana";
+  const index = fruits.indexOf(current);
+  console.log(index)*/
+
+  //slice
+
+
+  /*const firstThree = fruits.slice(0, 3);
+  console.log(firstThree)*/
+}
+
+
+
+export default App
